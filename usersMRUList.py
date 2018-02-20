36d671910f8eb4f907ff4615a5459aa6af7af0d9
Pacmanator <mrpacmanator@gmail.com>
2018-02-20 02:04:54 -0500

    Added GPLv3 License.

--- usersMRUList.py
+++ usersMRUList.py
@@ -2,6 +2,30 @@ import datetime
 import time
 import winreg as reg
 
+
+__author__ = "pacmanator"
+__email__ = "mrpacmanator@gmail.com"
+__version__ = "v1.0"
+
+"""
+    Python script to print a list of the Most Recently Used docs by some user.
+    Copyright (C) 2018 pacmanator
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
 # Not readable characters to name a file or directory
 NOT_READABLE_CHARS = [chr(character) for character in range(0, 32)]
 NOT_READABLE_CHARS += [chr(character) for character in range(33, 40)]
