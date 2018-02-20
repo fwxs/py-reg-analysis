ada36980259c859c42272c54eac4e70d176dad46
Pacmanator <mrpacmanator@gmail.com>
2018-02-20 02:02:49 -0500

    Minor changes in function network_list and dates functions.

--- networkList.py
+++ networkList.py
@@ -4,7 +4,7 @@ import winreg as reg
 
 __author__ = "pacmanator"
 __email__ = "mrpacmanator@gmail.com"
-__version__ = "v1.0"
+__version__ = "v1.1"
 
 """
     Python script to print a list of the previously connected networks.
@@ -30,26 +30,19 @@ def decode_mac_address(encoded_mac_address):
     return ":".join(["{:02x}".format(ch) for ch in encoded_mac_address])
 
 
-def get_first_connected_date(guid):
-    """ This function returns a readable UTC type time, specifying
-        when was created that network connection.
-    """
-    path = "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\NetworkList\\Profiles\\{0}".format(guid)
-
-    with reg.OpenKeyEx(reg.HKEY_LOCAL_MACHINE, path, 0, reg.KEY_READ) as key:
-        (n, date, t) = reg.EnumValue(key, 4)
-        return utc.get_utc(date)
-
-
-def get_last_connected(guid):
+def get_connected_dates(guid):
     """
         This function returns a readable UTC type time, specifying
-        when was the last time that some network connection was used.
+        when was the last time that some network connection was used
+        and the date when it was created.
+        @param guid: Connection Guid.
     """
     path = "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\NetworkList\\Profiles\\{0}".format(guid)
     with reg.OpenKeyEx(reg.HKEY_LOCAL_MACHINE, path, 0, reg.KEY_READ) as key:
-        (n, date, t) = reg.EnumValue(key, 6)
-        return utc.get_utc(date)
+        last_connected_date = reg.QueryValueEx(key, "DateLastConnected")[0]
+        first_connected_date = reg.QueryValueEx(key, "DateCreated")[0]
+
+        return utc.get_utc(last_connected_date), utc.get_utc(first_connected_date)
 
 
 def network_list():
@@ -70,27 +63,23 @@ def network_list():
                 # Open the specified subkey.
                 with reg.OpenKeyEx(key, reg.EnumKey(key, i), 0, reg.KEY_READ) as sub_key:
 
-                    # Iterates over all the values of the provided subkey.
-                    for j in range(reg.QueryInfoKey(sub_key)[1]):
+                    name = reg.QueryValueEx(sub_key, "Description")[0]
 
-                        guid = reg.EnumValue(sub_key, 0)[1]
-                        (n, name, t) = reg.EnumValue(sub_key, 4)
-                        (desc, addr, t) = reg.EnumValue(sub_key, 5)
+                    if name in names:
+                        continue
 
-                        if name in names:
-                            continue
+                    guid = reg.QueryValueEx(sub_key, "ProfileGuid")[0]
+                    mac_address = reg.QueryValueEx(sub_key, "DefaultGatewayMac")[0]
 
-                        # Get the date of the first and the last connection.
-                        first_conx = get_first_connected_date(guid)
-                        last_conx = get_last_connected(guid)
+                    first_conx, last_conx = get_connected_dates(guid)
 
-                        names.append(name)
-                        mac_address = decode_mac_address(addr) if addr is not None else ""
+                    names.append(name)
+                    mac_address = decode_mac_address(mac_address) if mac_address is not None else None
 
-                        yield name, mac_address, first_conx, last_conx
+                    yield name, mac_address, first_conx, last_conx
 
     except PermissionError:
-        print("You need to have admin privileges to read 'HKEY_LOCAL_MACHINE\{0}' key".format(path))
+        print("You need to have admin privileges to open the network lists key")
         sys.exit(5)
 
 
